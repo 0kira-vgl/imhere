994c4bb867a33415ba0df9684a032a45d9397eb8
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-10-08 22:35:46 -0300

    add .npmrc

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,29 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, Pressable } from "react-native";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("AddParticipant");
+  }
+
   return (
     <View className="flex-1 bg-[#131016] p-6">
       <Text className="text-2xl font-bold text-white mt-12">Seu evento</Text>
       <Text className="text-[#6B6B6B]">Sexta, 4 de Outubro de 2024</Text>
 
-      <TextInput
-        placeholder="Nome do participante"
-        className="h-[56] p-4 text-zinc-50 text-xl bg-[#1F1E25] rounded-md"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View className="flex-row gap-2.5 mt-9 mb-10">
+        <TextInput
+          placeholder="Nome do participante"
+          className="h-16 flex-1 p-4 text-zinc-50 text-xl bg-[#1F1E25] rounded-md"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <Pressable
+          onPress={handleParticipantAdd}
+          className="size-16 rounded-md bg-[#31CF67] flex items-center justify-center"
+        >
+          <Text className="text-white text-2xl">+</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
